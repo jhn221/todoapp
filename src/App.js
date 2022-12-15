9c6224ffbd37df8b63b3902a177919511ee514a9
janghanna <ewal4291@gmail.com>
2022-12-15 13:00:18 +0900

    live time 관련 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import useFetch from './util/useFetch'
--- src/components/NowDate.js
+++ src/components/NowDate.js
@@ -5,40 +5,38 @@ import Date from "react-live-clock";
 
 // 날짜 자동으로 바꿔주는 라이브러리 수정
 function NowDate () {
-
-
     return (
         <div className="year-month">
             <span>
                 <Year
                 id='year'
                 format={"YYYY"}
-                ticking={false}
-                timezone={'KR/Pacific'}
+                ticking={true}
+                timezone={'Asia/Seoul'}
                 />
             </span>.
             <span>
                 <Month
                 id='Month'
                 format={"MM"}
-                ticking={false}
-                timezone={'KR/Pacific'}
+                ticking={true}
+                timezone={'Asia/Seoul'}
                 />
             </span>.
             <span>
                 <Date
-                id='Date'
+                id='Date' 
                 format={"DD"}
-                ticking={false}
-                timezone={'KR/Pacific'}
+                ticking={true}
+                timezone={'Asia/Seoul'}
                 />
             </span>.
             <span>
                 <Day
                 id='Day'
                 format={"ddd"}
-                ticking={false}
-                timezone={'KR/Pacific'}
+                ticking={true}
+                timezone={'Asia/Seoul'}
                 />
             </span>
         </div>
